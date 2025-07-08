b255a40e2b1270079807d48f7dd87ead35cb835a
$𝖎𝖑𝖛𝖊𝖗𝖃 <serainox@gmail.com>
2025-07-08 11:08:18 +0200

    Delete SCDOS-1.7 directory

--- SCDOS-1.7/EXPERIMENTAL/ascii.ps1
+++
@@ -1,5 +0,0 @@
-clear
-Write-Host "cat Random ASCII Art`n"
-$files = Get-ChildItem -Path "C:\Shell\ascii\" -Filter *.txt
-$randomFile = Get-Random -InputObject $files
-Get-Content $randomFile.FullName
--- SCDOS-1.7/EXPERIMENTAL/update.ps1
+++
@@ -1,2 +0,0 @@
-      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
-     1121       1    1121       5456  cons0     197609 06:10:59 /usr/bin/ps
